9aaa6bc35f76d8c673c4dba48e3ce95505724681
Laurent Montel <montel@kde.org>
2026-01-05 13:42:33 +0100

    don't export class (it's not installed)

--- src/korganizereditorconfig.h
+++ src/korganizereditorconfig.h
@@ -5,73 +5,34 @@
 */
 
 #pragma once
-
 #include "editorconfig.h"
+#include "incidenceeditor_private_export.h"
 
 #include <KCalendarCore/IncidenceBase>
 
 namespace IncidenceEditorNG
 {
-/*!
- * \brief The KOrganizerEditorConfig class
- */
-class INCIDENCEEDITOR_EXPORT KOrganizerEditorConfig : public IncidenceEditorNG::EditorConfig
+class INCIDENCEEDITOR_TESTS_EXPORT KOrganizerEditorConfig : public IncidenceEditorNG::EditorConfig
 {
 public:
-    /*!
-     */
     KOrganizerEditorConfig();
-    /*!
-     */
     ~KOrganizerEditorConfig() override;
 
-    /*!
-     */
     KConfigSkeleton *config() const override;
-    /*!
-     */
     [[nodiscard]] QString fullName() const override;
-    /*!
-     */
     [[nodiscard]] QString email() const override;
-    /*!
-     */
     [[nodiscard]] bool thatIsMe(const QString &email) const override;
-    /*!
-     */
     [[nodiscard]] QStringList allEmails() const override;
-    /*!
-     */
     [[nodiscard]] QList<Organizer> allOrganizers() const override;
-    /*!
-     */
     [[nodiscard]] bool showTimeZoneSelectorInIncidenceEditor() const override;
-    /*!
-     */
     [[nodiscard]] QDateTime defaultDuration() const override;
-    /*!
-     */
     [[nodiscard]] QDateTime startTime() const override;
-    /*!
-     */
     [[nodiscard]] bool defaultAudioFileReminders() const override;
-    /*!
-     */
     [[nodiscard]] QUrl audioFilePath() const override;
-    /*!
-     */
     [[nodiscard]] int reminderTime() const override;
-    /*!
-     */
     [[nodiscard]] int reminderTimeUnits() const override;
-    /*!
-     */
     [[nodiscard]] bool defaultTodoReminders() const override;
-    /*!
-     */
     [[nodiscard]] bool defaultEventReminders() const override;
-    /*!
-     */
     [[nodiscard]] QStringList &templates(KCalendarCore::IncidenceBase::IncidenceType type) override;
 };
 } // IncidenceEditors
